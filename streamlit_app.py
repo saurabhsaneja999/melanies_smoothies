b75aa9f918c87a2a7487dac52350bdcc643fab83
saurabhsaneja999 <saurabh.saneja@ricoh-usa.com>
2025-06-17 18:42:00 +0530

    Update streamlit_app.py
    Rearranged line for pandas import

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 # Import python packages
 import streamlit as st
+import pandas as pd
 from snowflake.snowpark.functions import col
 import requests
-import pandas as pd
+
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your Smoothie! :cup_with_straw:")
